83a8f0dda50c297b292b8c99d50cd15a3c171068
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-21 16:59:56 +0200

    [add] middleware

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-  <div class="container">
+  <div class="container my-5">
     <table class="table">
       <thead>
         <tr>
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('project', ProjectController::class);
+
+Route::middleware(['auth', 'verified'])
+    ->group(function () {
+        // inserire tutte le root protette da author
+        Route::resource('project', ProjectController::class);
+    });
 
 require __DIR__ . '/auth.php';
